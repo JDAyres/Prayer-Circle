9ace8c3029f0926130f212b9c3883dad48735fe7
Jdayres <justin5208@me.com>
2023-10-10 20:12:26 -0400

    Oops...

--- app/userSettings.js
+++ app/userSettings.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { SafeAreaView, Text, View, StatusBar } from 'react-native';
 import { styled } from 'nativewind';
-import { Button } from '../../components/Buttons';
+import { Button } from '../components/Buttons';
 
 const StyledSafeArea = styled(SafeAreaView);
 const StyledView = styled(View);
@@ -9,15 +9,16 @@ const StyledText = styled(Text);
 
 export default function Page() {
 	return (
+
 		<StyledSafeArea className='bg-offblack border' style={{ flex: 1 }}>
-			<StyledView className='flex-1 items-center mt-20 pt-10'>
+			<StyledView className='flex-1 items-center mt-45 pt-10'>
 				<StyledText className='text-3xl text-white text-center tracking-widest leading-10'>
-					Profile
+					Settings
 				</StyledText>
 				<Button 
-					icon='settings'
-					href='/userSettings'
-					btnStyles={'absolute bottom-10 right-5'}
+					icon='person-circle-outline'
+					href='/profile'
+					btnStyles={'absolute bottom-10 left-5'}
 					width={'w-[60px]'}
 					height={'h-[60px]'}
 					iconSize={35}
--- backend/config.js
+++ backend/config.js
@@ -13,6 +13,7 @@ import { useRouter } from 'expo-router';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
 	apiKey: process.env.FIREBASE_API_KEY,
 	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
